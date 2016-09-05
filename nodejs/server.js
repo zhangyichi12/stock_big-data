53321fa1f5982974c2929a10474faac614cbbe31
Yichi Zhang <zhangyichi12@gmail.com>
2016-09-05 16:59:04 -0400

    add react-front-end

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,8 +1,13 @@
 import express from 'express';
 const app = express();
 
-import redis from 'redis';
+import { Server } from 'http';
+const server = Server(app);
 
+import socketio from 'socket.io';
+const io = socketio(server);
+
+import redis from 'redis';
 const redis_host = process.env.REDIS_HOST || '192.168.99.100';
 const redis_port = process.env.REDIS_PORT || '6379';
 const subscribe_channel = process.env.SUBSCRIBE_CHANNEL || 'spark-average-stock-price';
@@ -14,7 +19,10 @@ redisClient.subscribe(subscribe_channel);
 
 redisClient.on('message', (channel, message) => {
     console.log(`message received ${message}`);
+    io.sockets.emit('data', message);
 });
+
+app.use(express.static(__dirname + '/public'));
 
 //Start server
 const serverPort = process.env.SERVER_PORT || 3001
---
+++ react/README.md
@@ -0,0 +1,11 @@
+## React Proj
+
+## Setup guide
+
+1. Install NodeJS and pull code from:
+`https://github.com/zhangyichi12/Video-Weather-Note_Client.git`
+2. In the root folder run `npm intall`.
+
+## Usage
+1. If you have Google Chrome and your `8080` port is not being taken, you can run `npm start` and webpage will pop up. Enjoy.
+2. Or you can use `SERVER_PORT` to set the port you want to use.
